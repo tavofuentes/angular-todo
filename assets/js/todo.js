328bad487b6eafefc00739d4bb8f5e989ca3a784
Octavio Fuentes <octavio@mintitmedia.com>
2014-05-27 01:04:31 -0500

    add items works with progress bar for done items

--- assets/js/todo.js
+++ assets/js/todo.js
@@ -1,10 +1,28 @@
 var angulartodo = angular.module('angulartodo', []);
 
 angulartodo.controller('todoController', function ($scope) {
-  $scope.todos = [
-	{text:'learn angular', done:true},
-	{text:'build an angular app', done:false}
-  ];
+	$scope.todoItems = [
+	];
+
+	$scope.addItem = function (){
+		$scope.todoItems.push({ text: $scope.newItem, done : false });
+		$scope.newItem = '';
+	};
+
+	$scope.anyItems = function(){
+		if ($scope.todoItems.length)
+			return true;
+		else
+			return false;
+	};
+
+	$scope.getDonePercentaje = function(){
+		done_total = 0;
+		for (var i = 0; i < $scope.todoItems.length; i++)
+			if ($scope.todoItems[i].done)
+				done_total++;
+		return parseInt( (done_total/$scope.todoItems.length)*100);
+	};
 
 });
 
